73f4596534edc7b710edb5eee4cdf51a83f5f89e
Adam <NayJi7@github.com>
2025-03-29 16:37:10 +0100

    login form

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,54 +1,93 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger, SidebarInset } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/ui/app-sidebar"
-import { Separator } from "@/components/ui/separator"
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+import { useEffect } from "react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useNavigate } from "react-router-dom"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { TabItem, Tabs } from "flowbite-react";
+import { HiAdjustments, HiClipboardList, HiUserCircle } from "react-icons/hi";
+import { MdDashboard } from "react-icons/md";
+
 
 export default function HomePage({ children }: { children?: React.ReactNode }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Ajouter la classe home-page au body lors du montage du composant
+    document.body.classList.add('home-page');
+    
+    // La retirer lors du démontage du composant
+    return () => {
+      document.body.classList.remove('home-page');
+    };
+  }, []);
+
+  const handleLogout = () => {
+    // Ici vous pourriez ajouter du code pour supprimer le token d'authentification
+    // localStorage.removeItem('authToken');
+    // sessionStorage.removeItem('authToken');
+    
+    // Redirection vers la page de connexion
+    navigate('/login');
+  };
+
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="w-full h-auto">
+      <SidebarInset>
         <div className="w-full flex justify-between items-center">
-          <div className="container p-4 flex gap-4">
-            <SidebarTrigger />
+          <div className="flex-1 p-4 flex gap-4">
+            <SidebarTrigger className="mt-3 cursor-pointer" />
             {children}
+            
+            <Tabs className="w-full" aria-label="Onglets avec icônes" variant="underline">
+              <TabItem className="cursor-pointer" title="Tableau de bord" icon={MdDashboard}>
+                Ceci est <span className="font-medium text-gray-800 dark:text-white">le contenu associé à l'onglet Tableau de bord</span>.
+                Cliquer sur un autre onglet basculera la visibilité de celui-ci pour le suivant. Le JavaScript de l'onglet 
+                échange les classes pour contrôler la visibilité et le style du contenu.
+              </TabItem>
+              <TabItem className="cursor-pointer" title="Paramètres" icon={HiAdjustments}>
+                Ceci est <span className="font-medium text-gray-800 dark:text-white">le contenu associé à l'onglet Paramètres</span>.
+                Cliquer sur un autre onglet basculera la visibilité de celui-ci pour le suivant. Le JavaScript de l'onglet 
+                échange les classes pour contrôler la visibilité et le style du contenu.
+              </TabItem>
+              <TabItem className="cursor-pointer" title="Contacts" icon={HiClipboardList}>
+                Ceci est <span className="font-medium text-gray-800 dark:text-white">le contenu associé à l'onglet Contacts</span>.
+                Cliquer sur un autre onglet basculera la visibilité de celui-ci pour le suivant. Le JavaScript de l'onglet 
+                échange les classes pour contrôler la visibilité et le style du contenu.
+              </TabItem>
+              <TabItem disabled title="Désactivé">
+                Contenu désactivé
+              </TabItem>
+            </Tabs>
 
-            <Breadcrumb className="mt-1">
-              <BreadcrumbList>
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-
-          <div className="p-4 flex gap-4 w-min">
-            <Avatar>
-              <AvatarImage src="https://lh3.googleusercontent.com/ogw/AF2bZyhd184Wz5LlpcbpEbmK8TIg73_K9X5kKiP_EvFsDVHit4Gj=s32-c-mo" />
-              <AvatarFallback className="bg-gray-300">AT</AvatarFallback>
-            </Avatar>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="mt-3 rounded-full h-8">
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="https://lh3.googleusercontent.com/ogw/AF2bZyhd184Wz5LlpcbpEbmK8TIg73_K9X5kKiP_EvFsDVHit4Gj=s32-c-mo" />
+                  <AvatarFallback className="bg-gray-300">AT</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Mon Compte</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="cursor-pointer">Profil</DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">Facturation</DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">Équipe</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="cursor-pointer" onClick={handleLogout}>Déconnexion</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
-        
-        <div className="px-5"><Separator /></div>
-
-      </main>
+      </SidebarInset>
     </SidebarProvider>
   )
 }
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,35 +1,52 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Particles from '../blocks/Backgrounds/Particles/Particles.tsx';
 import SpotlightCard from '../blocks/Components/SpotlightCard/SpotlightCard.tsx';
-import { Checkbox, Label, TextInput } from "flowbite-react";
-import { MailOpen } from "lucide-react";
+import { Label, TextInput } from "flowbite-react";
+import { MailOpen, ArrowRight, KeyRound, Check, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import BlurText from "../blocks/TextAnimations/BlurText/BlurText.tsx";
-import { Separator } from "@/components/ui/separator"
+import SplitText from "../blocks/TextAnimations/SplitText/SplitText.tsx";
+import { Separator } from "@/components/ui/separator";
 import { HiMail } from "react-icons/hi";
 import { TbLockPassword } from "react-icons/tb";
 import { FiRepeat } from "react-icons/fi";
 import { useIsMobile } from '@/hooks/use-mobile';
+import { animated, useTransition, AnimatedProps } from '@react-spring/web';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from "flowbite-react";
+import { motion } from 'framer-motion';
 
-const handleAnimationComplete = () => {
-  console.log('Animation completed!');
+const AnimatedDiv = animated.div as React.FC<AnimatedProps<React.HTMLAttributes<HTMLDivElement>>>;
+
+const formVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } }
 };
 
 export default function LoginPage() {
   const isMobile = useIsMobile();
+  const [authStep, setAuthStep] = useState('login');
+  const [email, setEmail] = useState('');
+  const [openModal, setOpenModal] = useState(false);
+  const [acceptedTerms, setAcceptedTerms] = useState(false);
+  
+  const transitions = useTransition(authStep, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  });
 
   useEffect(() => {
-    // Ajouter la classe login-page au body lors du montage du composant
     document.body.classList.add('login-page');
-    
-    // La retirer lors du démontage du composant
     return () => {
       document.body.classList.remove('login-page');
     };
   }, []);
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setAuthStep('verify');
+  };
+
   return (
-    // particles background 
     <div className="absolute inset-0 w-full h-screen overflow-hidden bg-black">
       <Particles
         particleColors={['#ffffff', '#ffffff']}
@@ -42,57 +59,148 @@ export default function LoginPage() {
         disableRotation={false}
       />
       <div className="absolute inset-0 flex items-center justify-center z-10 pointer-events-none">
-        {/* Image - affichée uniquement sur les écrans non-mobiles */}
-        {!isMobile && (
-          <div className='w-4/5 h-full items-center justify-center flex'>
-            <img className='w-auto h-11/12 rounded-2xl' src="https://www.lalibre.be/resizer/v2/BJYUKUYMC5HL7EZAHSL7B3MXFA.jpg?auth=98465b5b9263ff3b8dc1e34aadad1646214eb19c644f32c889f5a3d6f2c20fab&width=1200&height=800&quality=85&focal=3360%2C2240" alt="Prison Image" />
-          </div>
-        )}
-        {/* Login form */}
+        <div className='hidden xl:flex w-4/5 h-full items-center justify-center'>
+          <img className='w-auto h-11/12 rounded-2xl' src="https://www.lalibre.be/resizer/v2/BJYUKUYMC5HL7EZAHSL7B3MXFA.jpg?auth=98465b5b9263ff3b8dc1e34aadad1646214eb19c644f32c889f5a3d6f2c20fab&width=1200&height=800&quality=85&focal=3360%2C2240" alt="Prison Image" />
+        </div>
         <div className='w-full h-full flex items-center justify-center'>
-        <SpotlightCard className={`${isMobile ? 'w-[90%]' : 'w-[70%]'} max-w-4xl mx-auto flex flex-col md:flex-row justify-evenly pointer-events-auto`} spotlightColor="rgba(0, 229, 255, 0.2)">
-            <form className="flex flex-col gap-4 pointer-events-none ">
-              <BlurText
-                text="Log In"
-                delay={150}
-                animateBy="words"
-                direction="top"
-                onAnimationComplete={handleAnimationComplete}
-                className="text-5xl mb-auto mx-auto text-white"
-              />
-              <Separator className='bg-gray-400' />
-              <div>
-                <div className="mb-2 block">
-                  <Label className='text-white' htmlFor="password2">Your email</Label>
-                </div>
-                <TextInput className='pointer-events-auto' id="email2" type="email" icon={HiMail} placeholder="name@gmail.com" required shadow />
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label className='text-white' htmlFor="password2">Your password</Label>
-                </div>
-                <TextInput className='pointer-events-auto' id="password2" icon={TbLockPassword} type="password" required shadow />
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label className='text-white' htmlFor="repeat-password">Repeat password</Label>
-                </div>
-                <TextInput className='pointer-events-auto' id="repeat-password" icon={FiRepeat} type="password" required shadow />
-              </div>
-              <div className="flex items-center gap-2 pt-4">
-                <Checkbox className='pointer-events-auto' id="agree" />
-                <Label  htmlFor="agree" className="flex text-white">
-                  I agree with the&nbsp;
-                  <a href="#" className="text-cyan-600 hover:text-cyan-400 dark:text-cyan-500 pointer-events-auto">
-                    terms and conditions
-                  </a>
-                </Label>
-              </div>
-              <Button className='bg-black hover:bg-gray-900 cursor-pointer pointer-events-auto' type="submit">
-                <MailOpen className="mr-2" /> Log In
-              </Button>
-            </form>
-          </SpotlightCard>
+          {transitions((styles) => (
+            <AnimatedDiv style={{ ...styles, position: 'absolute', width: '100%', display: 'flex', justifyContent: 'center' }}>
+              {authStep === 'login' ? (
+                <SpotlightCard className={`${isMobile ? 'w-[90%]' : 'w-[70%]'} max-w-4xl mx-auto flex flex-col md:flex-row justify-evenly pointer-events-auto`} spotlightColor="rgba(0, 229, 255, 0.2)">
+                  <motion.form 
+                    className="flex flex-col gap-4 w-[80%] mx-auto" 
+                    onSubmit={handleSubmit}
+                    initial="hidden"
+                    animate="visible"
+                    variants={formVariants}
+                  >
+                    <SplitText
+                      text="Connexion"
+                      delay={150}
+                      className="text-5xl mb-auto mx-auto text-white"
+                    />
+                    <Separator className='bg-gray-400' />
+                    <div>
+                      <div className="mb-2 block">
+                        <Label className='text-white' htmlFor="email2">Votre email</Label>
+                      </div>
+                      <TextInput 
+                        className='pointer-events-auto' 
+                        id="email2" 
+                        type="email" 
+                        icon={HiMail} 
+                        placeholder="nom@gmail.com" 
+                        required 
+                        shadow
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                    <div>
+                      <div className="mb-2 block">
+                        <Label className='text-white' htmlFor="password2">Votre mot de passe</Label>
+                      </div>
+                      <TextInput className='pointer-events-auto' id="password2" icon={TbLockPassword} placeholder='•••' type="password" required shadow />
+                    </div>
+                    <div>
+                      <div className="mb-2 block">
+                        <Label className='text-white' htmlFor="repeat-password">Répéter le mot de passe</Label>
+                      </div>
+                      <TextInput className='pointer-events-auto' id="repeat-password" icon={FiRepeat} placeholder='•••' type="password" required shadow />
+                    </div>
+                    <div className="flex items-center gap-2 pt-4 w-full justify-center">
+                      <div className="flex items-center gap-2">
+                        {acceptedTerms ? (
+                          <Check className="w-4 h-4 text-green-500" />
+                        ) : (
+                          <X className="w-4 h-4 text-red-500" />
+                        )}
+                        <a 
+                          href="#" 
+                          className="text-sm text-blue-400 hover:text-blue-300 hover:underline"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            setOpenModal(true);
+                          }}
+                        >
+                          Conditions d'utilisation
+                        </a>
+                      </div>
+                    </div>
+                    <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
+                      <ModalHeader>Conditions d'utilisation</ModalHeader>
+                      <ModalBody>
+                        <div className="space-y-6">
+                          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            À moins d'un mois de l'entrée en vigueur des nouvelles lois sur la protection de la vie privée des consommateurs 
+                            de l'Union européenne, les entreprises du monde entier mettent à jour leurs accords de conditions d'utilisation.
+                          </p>
+                          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            Le Règlement Général sur la Protection des Données (RGPD) de l'Union européenne entre en vigueur le 25 mai et 
+                            vise à garantir un ensemble commun de droits en matière de données dans l'Union européenne. Il oblige les 
+                            organisations à informer les utilisateurs dès que possible des violations de données à haut risque qui 
+                            pourraient les affecter personnellement.
+                          </p>
+                        </div>
+                      </ModalBody>
+                      <ModalFooter>
+                        <Button className='cursor-pointer' onClick={() => {
+                          setAcceptedTerms(true);
+                          setOpenModal(false);
+                        }}>J'accepte</Button>
+                        <Button className='cursor-pointer' color="gray" onClick={() => {
+                          setAcceptedTerms(false);
+                          setOpenModal(false);
+                        }}>
+                          Je refuse
+                        </Button>
+                      </ModalFooter>
+                    </Modal>
+                    <Button className='bg-black hover:bg-gray-900 cursor-pointer pointer-events-auto' type="submit">
+                      <MailOpen className="mr-2" /> Se connecter
+                    </Button>
+                  </motion.form>
+                </SpotlightCard>
+              ) : (
+                <SpotlightCard className={`${isMobile ? 'w-[90%]' : 'w-[70%]'} max-w-4xl mx-auto flex flex-col justify-evenly pointer-events-auto`} spotlightColor="rgba(0, 229, 255, 0.2)">
+                  <motion.form 
+                    className="flex flex-col gap-4 w-[80%] mx-auto" 
+                    onSubmit={(e) => e.preventDefault()}
+                    initial="hidden"
+                    animate="visible"
+                    variants={formVariants}
+                  >
+                    <SplitText
+                      text="Vérification"
+                      delay={150}
+                      className="text-5xl mb-auto mx-auto text-white"
+                    />
+                    <Separator className='bg-gray-400' />
+                    <div className="text-center text-white mb-4">
+                      Un code de vérification a été envoyé à {email}
+                    </div>
+                    <div>
+                      <div className="mb-2 block">
+                        <Label className='text-white' htmlFor="verificationCode">Code de vérification</Label>
+                      </div>
+                      <TextInput 
+                        className='pointer-events-auto' 
+                        id="verificationCode" 
+                        type="text" 
+                        icon={KeyRound}
+                        placeholder="Entrez le code à 6 chiffres" 
+                        required 
+                        shadow 
+                      />
+                    </div>
+                    <Button className='bg-black hover:bg-gray-900 cursor-pointer pointer-events-auto mt-4' type="submit">
+                      <ArrowRight className="mr-2" /> Vérifier
+                    </Button>
+                  </motion.form>
+                </SpotlightCard>
+              )}
+            </AnimatedDiv>
+          ))}
         </div>
       </div>
     </div>
