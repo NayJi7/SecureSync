0297a28710cac68b794399b382c27d6ba169d71f
anthonyvsn <anthony.voisin.78130@hotmail.com>
2025-04-03 14:40:45 +0200

    deleted back before merge

--- backend/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.home_view, name='home'),
-    # Vous pouvez ajouter d'autres routes ici selon vos besoins
-]
