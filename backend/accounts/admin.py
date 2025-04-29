7982f41e5d8c3707cd727729c5b2d513c881c288
Adam <NayJi7@github.com>
2025-04-29 18:08:00 +0200

    added prison to admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,8 +1,9 @@
 # accounts/admin.py
 from django.contrib import admin
-from .models import CustomUser  # Si tu as un modèle User personnalisé, sinon supprime cette ligne
+from .models import CustomUser, Prison # Si tu as un modèle User personnalisé, sinon supprime cette ligne
 from .models import OTPCode, UserActivityLog
 
 admin.site.register(CustomUser)  # Enregistrer le modèle User dans l'admin
 admin.site.register(OTPCode)
 admin.site.register(UserActivityLog)
+admin.site.register(Prison)
