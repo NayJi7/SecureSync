a927cc27b20d472bf19890c11a2d82ed3f1b2dc1
Adam <NayJi7@github.com>
2025-04-30 19:38:29 +0200

    redirect render

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,12 @@
 # myproject/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),  # Changé de '' à 'api/' pour s'assurer que les URLs commencent par /api/
     path('api/', include('statistique.urls')),
     path('api/', include('object.urls')),
-   
+    path("", lambda request: HttpResponseRedirect("http://localhost:5173/")),
 ]
